12d2f031de77b0d254143717d7ef203dedbd0cab
hugoesteban <108495976+hugoesteban@users.noreply.github.com>
2022-10-19 22:35:09 -0300

    Add files via upload

---
+++ M1-nuevocodigo.js
@@ -0,0 +1,3 @@
+console.log("¡Hola Mundo! Este es mi primer Script"); //muestra mensaje en consola
+document.write("<br/>ejemplos de cómo incluir JavaScript en un documento HTML"); //muestra mensaje en la página
+alert('Ver lo simple que es agregar "comillas dobles" y ‘comillas simples’'); //muestra una alerta
